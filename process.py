8a421600160f5ce4e46ca289c386bdc39d63ce66
Benedikt Holland <benedikt.holland@protonmail.com>
2023-01-22 16:26:05 +0100

    attempted noisereduce library

--- process.py
+++ process.py
@@ -3,22 +3,12 @@ import numpy as np
 from sklearn.decomposition import FastICA
 from sklearn.preprocessing import normalize
 import scipy.io.wavfile as wf
+import noisereduce as nr
+import wavfile
 
 if __name__ == '__main__':
-    input_wave = wave.open("input.wav", "r")
-    output_wave = wave.open("output.wav", "r")
-    fs_in = input_wave.getframerate()
-    fs_out = output_wave.getframerate()
-    input_raw = input_wave.readframes(-1)
-    output_raw = output_wave.readframes(-1)
-    input = np.frombuffer(input_raw, 'int16')
-    output = np.frombuffer(output_raw, 'int16')
-    output = np.reshape(output, (-1, 2))
-    wf.write("output_edit.wav", fs_out, output)
-    output = output[:, 0]
-    output = output / np.linalg.norm(output)
-    input = input / np.linalg.norm(input)
-    result = input - output
-    wf.write("input_edit.wav", fs_in, 32767*100*result.astype("int16"))
-    
+    input, rate_in, bits_in = wavfile.read("input.wav")
+    output, rate_out, bits_out = wavfile.read("output.wav")
+    result = nr.reduce_noise(np.array(input), sr=rate_in, y_noise=np.array(output), stationary=True, tmp_folder="./tmp")
+    wavfile.write("input_edit.wav", result, rate_in, bits_in)
     
